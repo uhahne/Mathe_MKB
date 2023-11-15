74fa886cac5aa18a9b843900152da930049c3420
Uwe Hahne <uhahne@gmail.com>
2023-11-15 20:23:01 +0100

    cleaner code

--- drawing.js
+++ drawing.js
@@ -20,13 +20,28 @@ class CartesianCoordinateSystem2D {
 
     drawPositionVector(v, name ="", showName = true, showCoords = false, color = "black") {
         let from = this.origin;
-        let to = new Vector2D(this.origin.x + this.baseX.scale(v.x).x,
-                              this.origin.y + this.baseY.scale(v.y).y);
-        let headlen = Math.floor(this.scale/3); // length of head in pixels
+        let to = this.#transformToPixel(v);
+        this.#drawVector(to, from, color, showCoords, showName, name, v);
+    }
+
+    drawVector(v, origin, name ="", showName = true, showCoords = false, color = "black") {
+        let from = this.#transformToPixel(origin);
+        let to = this.#transformToPixel(v.add(origin));
+        this.#drawVector(to, from, color, showCoords, showName, name, v);
+    }
+
+    #drawVector(to, from, color, showCoords, showName, name, v) {
+        let headlen = Math.floor(this.scale / 3); // length of head in pixels
         let angle = Math.atan2(to.y - from.y, to.x - from.x);
         this.context.beginPath();
-        this.context.font = Math.floor(this.scale/3) + "px Arial";
+        this.context.font = Math.floor(this.scale / 3) + "px Arial";
         this.context.strokeStyle = color;
+        this.context.moveTo(from.x, from.y);
+        this.context.lineTo(to.x, to.y);
+        this.context.moveTo(to.x, to.y);
+        this.context.lineTo(to.x - headlen * Math.cos(angle - Math.PI / 6), to.y - headlen * Math.sin(angle - Math.PI / 6));
+        this.context.moveTo(to.x, to.y);
+        this.context.lineTo(to.x - headlen * Math.cos(angle + Math.PI / 6), to.y - headlen * Math.sin(angle + Math.PI / 6));
         if (showCoords && showName) {
             this.context.fillText(name + "(" + v.x.toFixed(2) + ", " + v.y.toFixed(2) + ")", to.x + 10, to.y - 10);
         } else if (showName) {
@@ -34,13 +49,12 @@ class CartesianCoordinateSystem2D {
         } else if (showCoords) {
             this.context.fillText("(" + v.x.toFixed(2) + ", " + v.y.toFixed(2) + ")", to.x + 10, to.y - 10);
         }
-        this.context.moveTo(from.x, from.y);
-        this.context.lineTo(to.x, to.y);
-        this.context.moveTo(to.x, to.y);
-        this.context.lineTo(to.x - headlen * Math.cos(angle - Math.PI / 6), to.y - headlen * Math.sin(angle - Math.PI / 6));
-        this.context.moveTo(to.x, to.y);
-        this.context.lineTo(to.x - headlen * Math.cos(angle + Math.PI / 6), to.y - headlen * Math.sin(angle + Math.PI / 6));
         this.context.stroke();
+    }
+
+    #transformToPixel(origin) {
+        return new Vector2D(this.origin.x + this.baseX.scale(origin.x).x,
+            this.origin.y + this.baseY.scale(origin.y).y);
     }
 
     drawBaseAxes() {
@@ -49,7 +63,7 @@ class CartesianCoordinateSystem2D {
         this.drawPositionVector(new Vector2D(0,1),"y");
     };
 
-    drawLine(from, to, color = "black") {
+    #drawLine(from, to, color = "black") {
         this.context.beginPath();
         this.context.lineWidth = this.scale / 10;
         this.context.strokeStyle = color;
@@ -60,10 +74,23 @@ class CartesianCoordinateSystem2D {
     
     drawGrid(spacing, color = "black") {
         for (let i = 0; i < this.canvas.width; i += spacing) {
-            this.drawLine(new Vector2D(i, 0), new Vector2D(i, this.canvas.height), color);
+            this.#drawLine(new Vector2D(i, 0), new Vector2D(i, this.canvas.height), color);
         }
         for (let i = 0; i < this.canvas.height; i += spacing) {
-            this.drawLine(new Vector2D(0, i), new Vector2D(this.canvas.width, i), color);
+            this.#drawLine(new Vector2D(0, i), new Vector2D(this.canvas.width, i), color);
         }
+    }
+
+    drawPoint(v, name = "", showCoords = false, color = "#" + Math.floor(Math.random()*16777215).toString(16)) {
+        let p = new Vector2D(this.origin.x + this.baseX.scale(v.x).x,
+                              this.origin.y + this.baseY.scale(v.y).y);
+        this.context.beginPath();
+        this.context.fillStyle = color;
+        this.context.strokeStyle = color;
+        this.context.arc(p.x, p.y, 5, 0, 2 * Math.PI);
+        if (showCoords) {
+            this.context.fillText(name + "(" + v.x.toFixed(2) + ", " + v.y.toFixed(2) + ")", p.x + 10, p.y - 10);
+        }
+        this.context.fill();
     }
 }
--- main.js
+++ main.js
@@ -4,31 +4,43 @@ resizeCanvas();
 
 // main drawing function
 function drawStuff() {
-    let scale = 80; // pixels per unit
-    // find best origin of canvas, close to center but on grid
-    let xOffset = Math.floor(canvas.width / 2) % scale;
-    let yOffset = Math.floor(canvas.height / 2) % scale;
-    let origin = new Vector2D(Math.floor(canvas.width / 2) - xOffset, Math.floor(canvas.height / 2) - yOffset);
-    let baseX = new Vector2D(scale, 0); // first base vector
-    let baseY = new Vector2D(0, -scale); // second base vector
-    let context = canvas.getContext("2d");
-
-    let coordinateSystem = new CartesianCoordinateSystem2D(origin, baseX, baseY, scale, context, canvas);
-    coordinateSystem.drawGrid(scale, "lightgray");
-    coordinateSystem.drawBaseAxes();
+    let cs = createAndDrawCoordinateSystem();
 
     let vec1 = new Vector2D(-1, 3);
     let vec2 = new Vector2D(3, 2);
     let vec3 = vec1.add(vec2);
+    let M = vec3.scale(0.5);
 
     console.log(vec1);
-    coordinateSystem.drawPositionVector(vec1, "V1", true, true, "blue");
-    coordinateSystem.drawPositionVector(vec2, "V2", true, true, "green");
-    coordinateSystem.drawPositionVector(vec3, "V3", true, true, "red");
+    cs.drawPositionVector(vec1, "V1", true, true, "blue");
+    cs.drawPositionVector(vec2, "V2", true, true, "green");
+    cs.drawPositionVector(vec3, "V3", true, true, "red");
+    cs.drawVector(vec1, vec2, "V1", false, false, "lightblue");
+    cs.drawPoint(M, "M", true, "black");
 }
 
 // event listener for canvas size changes
 window.addEventListener('resize', resizeCanvas, false);
+
+function createAndDrawCoordinateSystem() {
+    let scale = 80; // pixels per unit
+
+    // find best origin of canvas, close to center but on grid
+    let xOffset = Math.floor(canvas.width / 2) % scale;
+    let yOffset = Math.ceil(canvas.height / 2) % scale;
+    let origin = new Vector2D(Math.floor(canvas.width / 2) - xOffset, Math.ceil(canvas.height / 2) - yOffset);
+    // define base vectors
+    let baseX = new Vector2D(scale, 0); 
+    let baseY = new Vector2D(0, -scale); // negative y flips axis
+    // get context for drawing
+    let context = canvas.getContext("2d");
+
+    // create coordinate system
+    let coordinateSystem = new CartesianCoordinateSystem2D(origin, baseX, baseY, scale, context, canvas);
+    coordinateSystem.drawGrid(scale, "lightgray");
+    coordinateSystem.drawBaseAxes();
+    return coordinateSystem;
+}
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
