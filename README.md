9da9b0f047e58a9665541725ec7a0ae14a796ed4
Uwe Hahne <uhahne@gmail.com>
2024-01-10 17:40:18 +0100

    Added three.js example from the lecture.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This repository contains supporting code for the lecture MaGrCGGe (MKB2) in the winter term 2023/24
 
 ## Usage
-The code has been developed and demonstrated using [Visual Studio Code](https://code.visualstudio.com/download). Settings and extension recommendations are committed.
+The code has been developed and demonstrated using [Visual Studio Code](https://code.visualstudio.com/download). Settings and extension recommendations are committed. Make yourself familiar with the [terminal in VS Code](https://code.visualstudio.com/docs/terminal/basics).
 
 ## Vector and matrix calculus in 2D
 Open [index.html](./index.html) using the live server extension. Change the ```drawStuff()``` method in [main.js](./main.js) in order to experiment with vectors and matrices in 2D. The implementation of the mathematical functions (add, scale, ...) is done in [math.js](./math.js).
@@ -11,5 +11,40 @@ Open [index.html](./index.html) using the live server extension. Change the ```d
 Open [rotation.html](./rotation.html) using the live server extension. Move the slider to rotate the image. Below the image, the rotation matrix is shown, once with numbers and once with sine and cosine of the rotation angle.
 
 ## 3D cameras in WebGL with Three.JS
-tbd.
+### Installation
+The code is based on the three.js hello world application, created by following this [installation guide](https://threejs.org/docs/index.html#manual/en/introduction/Installation). In order to use the code [Node.js](https://nodejs.org/) needs to be installed. You can check if it is already installed by typing:
+```
+node --version
+```
+into the terminal.
+Then install three.js and vite via npm as recommended by three.js by typing:
+```
+npm install 
+```
+(The package.json is already committed so that this command is enough.)
+
+From your terminal, go to the camerasThreeJS subfolder and run vite:
+```
+cd camerasThreeJS
+npx vite
+```
+This starts a local vite server. If everything went well, you'll see a URL like http://localhost:5173 appear in your terminal, and can open that URL to see your web application. Every time the code is changed and saved, the page is reloaded automatically. You can stop the server by pressing Ctrl-C in the terminal.
+
+### Usage
+The application renders a colored cube on the screen with a fixed size of 1000 by 1000 pixels using a perspective camera. There are the following interactions possible:
+- [Orbit controls](https://threejs.org/docs/index.html#examples/en/controls/OrbitControls) to change the camera. You can rotate the cube with the left mouse button. Note that mathematically the camera is orbiting the cube. You can translate the camera with the arrow keys. It is also possible to zoom with the mouse wheel.
+- Pressing 'o' switches to an orthographic camera.
+- Pressing 'p' switches to an perspective camera.
+- Pressing 'f' switches between the fixed screen size and window screen size.
+- Pressing 'q' starts and stops a rotation of the cube.
+For most of those interactions the corresponding transformation matrix is printed on the console. Open your browsers developer tools (usually F12) to see the matrix values.
+
+### Code hints
+The code is demonstrated in the lecture in order to explain how the projection matrices for both orthographic and perspective cameras are defined. It is highly recommended to change the code in order to understand the topic. Here are the most important lines of code listed:
+- Change the screen size in *line 4* if your monitor resp. browser window is not large enough to display 1000 by 1000 pixels.
+- The camera parameters are defined in *lines 12ff*.
+  - Note that the camera position and orientation is set in the function `setupCamera()` to z=3 so that the cube object which is placed in the origin can be seen. The camera parameter `rotation` is set to `(0,0,0)` which is the default setting and makes the camera "look" towards the negative z-axis.
+- In the *lines 97ff* the key events are handled.
+- In *line 83* the animation loop is called and the cube is rotated if the `doAnimate` variable is `true`.
+- In *line 71* the function `printMatrix(matrix)` is defined which prints out matrix values on the console. Note that three.js internally stores matrices in column-major order. 
 
---
+++ camerasThreeJS/main.js
@@ -0,0 +1,158 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const fixedScreenSize = 1000;
+var canvasSizeIsFixed = true;
+let SCREEN_WIDTH = window.innerWidth;
+let SCREEN_HEIGHT = window.innerHeight;
+let aspect = canvasSizeIsFixed ? 1.0 : SCREEN_WIDTH / SCREEN_HEIGHT;
+let doAnimate = false;
+
+const scene = new THREE.Scene();
+// camera parameters
+const left = -1 * aspect;
+const right = 1 * aspect;
+const top = 1;
+const bottom = -1;
+const near = 1;
+const far = 100;
+const cameraOrtho = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+const fovy = 90;
+const cameraPerspective = new THREE.PerspectiveCamera(fovy, aspect, near, far);
+setupCamera(cameraPerspective);
+setupCamera(cameraOrtho);
+
+let camera = cameraPerspective;
+
+
+const renderer = new THREE.WebGLRenderer();
+if (canvasSizeIsFixed) {
+    renderer.setSize(fixedScreenSize, fixedScreenSize);
+} else {
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+document.body.appendChild(renderer.domElement);
+
+const colors = [0x00ffff, 0x00ff00, 0x0000ff, 0xffff00, 0xff0000, 0xff00ff];
+
+const geometry = new THREE.BoxGeometry();
+const material = colors.map(color => new THREE.MeshBasicMaterial({ color }));
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// White directional light at half intensity shining from the top.
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+scene.add(directionalLight);
+
+const light = new THREE.AmbientLight(0xaaaaaa); // soft white light
+scene.add(light);
+
+// controls
+let orthoControls = new OrbitControls(cameraOrtho, renderer.domElement);
+let perspControls = new OrbitControls(cameraPerspective, renderer.domElement);
+setupControls(orthoControls);
+setupControls(perspControls);
+
+let controls = orthoControls;
+controls.listenToKeyEvents(window); // optional
+
+window.addEventListener('resize', onWindowResize);
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('mouseup', handleMouseUp, false);
+
+function handleMouseUp(event) {
+    // Your code for handling mouse release goes here
+    console.log("Camera (local) matrix:");
+    printMatrix(camera.matrix);
+    console.log("Cube matrix:");
+    printMatrix(cube.matrix);
+}
+
+function printMatrix(matrix) {
+    // Print the camera matrix with values rounded to two decimals in a 4x4 grid
+    for (let i = 0; i < 4; i++) {
+        let row = '';
+        for (let j = 0; j < 4; j++) {
+            const index = j * 4 + i; // values are stored column-major internally
+            row += matrix.elements[index].toFixed(2) + '\t';
+        }
+        console.log('Row', i, ':', row);
+    }
+}
+
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    if (doAnimate) {
+        cube.rotation.x += 0.00;
+        cube.rotation.y += 0.01;
+    }
+
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+    renderer.render(scene, camera);
+};
+
+animate();
+
+function onKeyDown(event) {
+    switch (event.keyCode) {
+        case 70: /*F*/
+            canvasSizeIsFixed = !canvasSizeIsFixed;
+            onWindowResize();
+            break;
+
+        case 79: /*O*/
+            camera = cameraOrtho;
+            console.log("Orthographic camera projection matrix:");
+            printMatrix(camera.projectionMatrix);
+            break;
+
+        case 80: /*P*/
+            camera = cameraPerspective;
+            console.log("Perspective camera projection matrix:");
+            printMatrix(camera.projectionMatrix);
+            break;
+
+        case 81: /*Q*/
+            doAnimate = !doAnimate;
+            console.log("Cube object matrix:");
+            printMatrix(cube.matrix);
+            break;
+    }
+
+}
+
+function onWindowResize() {
+
+    SCREEN_WIDTH = window.innerWidth;
+    SCREEN_HEIGHT = window.innerHeight;
+    aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+    camera.updateProjectionMatrix();
+    if (canvasSizeIsFixed) {
+        renderer.setSize(fixedScreenSize, fixedScreenSize);
+    } else {
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+
+}
+
+function setupCamera(cam) {
+    cam.rotation.set(0, 0, 0);
+    cam.position.x = 0;
+    cam.position.y = 0;
+    cam.position.z = 3;
+}
+
+function setupControls(ctrl) {
+    //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+    ctrl.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    ctrl.dampingFactor = 0.05;
+
+    ctrl.screenSpacePanning = false;
+
+    ctrl.minDistance = 1;
+    ctrl.maxDistance = 5;
+
+    ctrl.maxPolarAngle = Math.PI;
+}
